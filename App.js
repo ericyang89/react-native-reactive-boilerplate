9e572efe3f0dce9a69906b2ae4c1ba25e58505f1
yangchuan <yangchuan@douyu.tv>
2017-08-18 10:02:27 +0800

    init proj

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import FadeInView from './src/FadeInView';
+import Routes from './Routes';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Routes></Routes>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
