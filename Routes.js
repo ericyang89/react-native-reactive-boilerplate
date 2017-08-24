c3cfaaad07f1c2fb2fbf8f5424ea05f068ca6c40
yangchuan <yangchuan@douyu.tv>
2017-08-24 11:02:25 +0800

    add new repo

--- Routes.js
+++ Routes.js
@@ -1,115 +1,68 @@
-import React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
-import {NativeRouter, Route, Link} from 'react-router-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Route, Switch } from "react-router";
+import { NativeRouter, Link, AndroidBackButton } from "react-router-native";
+import { Navigation, Card } from "react-router-navigation";
 
-const Home = () => (
-  <Text style={styles.header}>
-    Home
-  </Text>
-)
+const Home = () => <Text style={styles.header}>Home</Text>;
+const About = () => <Text style={styles.header}>About</Text>;
+const Topics = () => <Text style={styles.header}>Topics</Text>;
 
-const About = () => (
-  <Text style={styles.header}>
-    About
-  </Text>
-)
-
-const Topic = ({match}) => (
+const Topic = ({ match }) =>
   <Text style={styles.topic}>
     {match.params.topicId}
-  </Text>
-)
+  </Text>;
 
-const Topics = ({match}) => {
-  console.log(444, match);
+const Routes = () => {
   return (
-    <View>
-      <Text style={styles.header}>Topics</Text>
+    <NativeRouter>
       <View>
-        <Link
-          to={`${match.url}/rendering`}
-          style={styles.subNavItem}
-          underlayColor='#f0f4f7'>
-          <Text>Rendering with React</Text>
-        </Link>
-        <Link
-          to={`${match.url}/components`}
-          style={styles.subNavItem}
-          underlayColor='#f0f4f7'>
-          <Text>Components</Text>
-        </Link>
-        <Link
-          to={`${match.url}/props-v-state`}
-          style={styles.subNavItem}
-          underlayColor='#f0f4f7'>
-          <Text>Props v. State</Text>
-        </Link>
+        <AndroidBackButton />
+        <Navigation>
+          <Card
+            exact
+            path="/"
+            render={() =>
+              <Link to="/hello">
+                <Text>Press it</Text>
+              </Link>}
+          />
+          <Card path="/hello" render={() => <Text>Hello</Text>} />
+        </Navigation>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
+        </Switch>
       </View>
-
-      <Route path={`${match.url}/:topicId`} component={Topic}/>
-      <Route
-        exact
-        path={match.url}
-        render={() => (
-        <Text style={styles.topic}>Please select a topic.</Text>
-      )}/>
-    </View>
+    </NativeRouter>
   );
-}
-
-const Component6 = () => (
-  <View>
-    <Text>this is componenets666</Text>
-  </View>
-)
-
-const Routes = () => (
-  <NativeRouter>
-    <View style={styles.container}>
-      <View style={styles.nav}>
-        <Link to="/" underlayColor='#f0f4f7' style={styles.navItem}>
-          <Text>Home</Text>
-        </Link>
-        <Link to="/about" underlayColor='#f0f4f7' style={styles.navItem}>
-          <Text>About</Text>
-        </Link>
-        <Link to="/topics" underlayColor='#f0f4f7' style={styles.navItem}>
-          <Text>Topics</Text>
-        </Link>
-      </View>
-
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topics}></Route>
-    </View>
-  </NativeRouter>
-)
-/*    {//<Route path="components" component={Component6}></Route>} */
+};
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 25,
-    padding: 10
+    padding: 10,
   },
   header: {
-    fontSize: 20
+    fontSize: 20,
   },
   nav: {
-    flexDirection: 'row',
-    justifyContent: 'space-around'
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
   navItem: {
     flex: 1,
-    alignItems: 'center',
-    padding: 10
+    alignItems: "center",
+    padding: 10,
   },
   subNavItem: {
-    padding: 5
+    padding: 5,
   },
   topic: {
-    textAlign: 'center',
-    fontSize: 15
-  }
-})
+    textAlign: "center",
+    fontSize: 15,
+  },
+});
 
-export default Routes
+export default Routes;
