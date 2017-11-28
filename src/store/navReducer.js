dfdddf7e963df5297869adb11dd1e7497a3a5c36
yangchuan <yangchuan@douyu.tv>
2017-11-28 21:09:40 +0800

    add init state

--- src/store/navReducer.js
+++ src/store/navReducer.js
@@ -1,3 +1,4 @@
+import { NavigationActions } from "react-navigation";
 import AppNavigator from "./../containers";
 
 // 还不理解这里
@@ -5,8 +6,11 @@ import AppNavigator from "./../containers";
 // const initialState = AppNavigator.router.getStateForAction(
 //   AppNavigator.router.getActionForPathAndParams("Home")
 // );
+const initialState = AppNavigator.router.getStateForAction(
+  NavigationActions.init()
+);
 
-const navReducer = (state, action) => {
+const navReducer = (state = initialState, action) => {
   const nextState = AppNavigator.router.getStateForAction(action, state);
   return nextState || state;
 };
